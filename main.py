4e89822f12e4e941ae4454751060fdf1f5784aa0
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-11-07 13:10:00 -0500

    solution of exercise 3

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-#Escribe un programa que determine si una nota numérica es "Aprobado" o "Reprobado" usando if
+#Utiliza match para implementar una calculadora simple.
+#Enunciado:
+#Crea una calculadora que solicite dos números y una operación matemática (+, -, *, /). Usa match
+#para realizar la operación correspondiente.
 
-nota = float(input("Introduce la nota: "))
+numero1 = float(input("enter the first number: "))
 
-if nota >= 60:
+operacion = input("enter the operator: ")
 
-    print("Aprobado")
+numero2 = float(input("enter the second number: "))
 
-else:
-
-    print("Reprobado")
+match operacion:
+    case '+':
+        resultado = numero1 + numero2
+        print(f"the result of {numero1} + {numero2} es {resultado}.")
+    case '-':
+        resultado = numero1 - numero2
+        print(f"the result of {numero1} - {numero2} es {resultado}.")
+    case '*':
+        resultado = numero1 * numero2
+        print(f"the result of {numero1} * {numero2} es {resultado}.")
+    case '/':
+        if numero2 != 0:
+            resultado = numero1 / numero2
+            print(f"the result of {numero1} / {numero2} es {resultado}.")
+        else:
+            print("error: cannot divide by zero.")
+    case _:
+        print("Invalid operation. Please enter a valid transaction(+, -, *, /).")
