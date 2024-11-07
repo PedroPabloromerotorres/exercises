12b157345a9a7c491cfb6ea53b38629768ec7a05
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-11-07 13:22:37 -0500

    solution of exercise 6

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-#Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
-#semana usando match .
-#Enunciado:
-#Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
-#7 = Domingo).
+#Escribe un programa que implemente un juego de adivinanza de números.Enunciado:
+#El programa genera un número aleatorio entre 1 y 10. El usuario debe adivinar el número, y el
+#programa indica si el número ingresado es mayor, menor o igual al número generado
 
-numero = int(input("Introduce un número del 1 al 7: "))
+import random
 
-match numero:
-    case 1:
-        print("Lunes")
-    case 2:
-        print("Martes")
-    case 3:
-        print("Miércoles")
-    case 4:
-        print("Jueves")
-    case 5:
-        print("Viernes")
-    case 6:
-        print("Sábado")
-    case 7:
-        print("Domingo")
-    case _:
-        print("Número inválido. Por favor, ingresa un número entre 1 y 7.")
+secret_number = random.randint(1, 10)
+
+print("Welcome to the number guessing game!")
+print("I'm thinking of a number between 1 and 10. Try to guess it.")
+
+
+while True:
+
+    guess = int(input("Enter your guess: "))
+    if guess < secret_number:
+        print("The number you guessed is lower. Try again.")
+    elif guess > secret_number:
+        print("The number you guessed is higher. Try again.")
+    else:
+        print(f"Congratulations! You guessed the number {secret_number}.")
+        break
