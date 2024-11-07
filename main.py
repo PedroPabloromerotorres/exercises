9acb22ff9048fb45d5537d0615debd87387208ac
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-11-07 12:44:05 -0500

    solution of exercise 1

---
+++ main.py
@@ -0,0 +1,10 @@
+#Escribe un programa que verifique si un número es par o impar utilizando if
+
+numero = int(input( "write a number: "))
+
+if numero % 2 == 0:
+    print(f"{numero} it's even")
+
+
+else:
+    print(f"{numero} is ood ")
