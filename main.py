c03bbebada39bea076f47c793a016048a7dbca72
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-11-07 13:18:38 -0500

    solution of exercise 5

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-#Escribe un programa que determine el tipo de triángulo en función de sus lados usando if .
+#Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
+#semana usando match .
 #Enunciado:
-#Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el
-#triángulo es equilátero, isósceles o escaleno
+#Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
+#7 = Domingo).
 
-side1 = float(input("Enter the length of the first side: "))
+numero = int(input("Introduce un número del 1 al 7: "))
 
-side2 = float(input("Enter the length of the second side: "))
-
-side3 = float(input("Enter the length of the third side: "))
-
-if side1 == side2 == side3:
-
-    print("The triangle is equilateral.")
-
-elif side1 == side2 or side1 == side3 or side2 == side3:
-
-    print("The triangle is isosceles.")
-
-else:
-
-    print("The triangle is scalene.")
+match numero:
+    case 1:
+        print("Lunes")
+    case 2:
+        print("Martes")
+    case 3:
+        print("Miércoles")
+    case 4:
+        print("Jueves")
+    case 5:
+        print("Viernes")
+    case 6:
+        print("Sábado")
+    case 7:
+        print("Domingo")
+    case _:
+        print("Número inválido. Por favor, ingresa un número entre 1 y 7.")
